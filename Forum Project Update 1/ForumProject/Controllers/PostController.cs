4e3d3c3f4431e45d4537a66a48ef43b97fc717d0
Plotso <ilian_1999n@abv.bg>
2017-04-24 02:33:05 +0300

    Added Authentication so that only the author of a specific post and the admins of the forum can see the edit/delete button. Also now you can't go to ".../Post/Create" if you are not logged in, it sends you to the login page. Also started adding the function with comments created the "Models/Comment.cs" and made changes connected to the comments in the "PostController.cs" and made some additional changes. The full commenting options will be added in future updates!

---
+++ Forum Project Update 1/ForumProject/Controllers/PostController.cs
@@ -0,0 +1,128 @@
+﻿using ForumProject.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ForumProject.Controllers
+{
+    public class PostController : Controller
+    {
+        // GET: Post
+        public ActionResult Index()
+        {
+            return RedirectToAction("AllPosts");
+        }
+
+        public ActionResult AllPosts()
+        {
+            using (var db = new ForumDbContext())
+            {
+                var posts = db.Posts
+                    .Include(p => p.Author)
+                    .ToList();
+
+                return View(posts);
+            }
+        }
+
+        public ActionResult ViewPost (int? id)
+        {
+            using (var db = new ForumDbContext())
+            {
+                var post = db.Posts
+                    .Include(p => p.Author)
+                    .Where(p => p.Id == id)
+                    .First();
+
+                if (post == null)
+                {
+                    return HttpNotFound();
+                }
+
+                return View(post);
+            }
+
+        }
+
+        [Authorize]
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db = new ForumDbContext())
+                {
+                    var authorId = User.Identity.GetUserId();
+                    post.AuthorId = authorId;
+
+                    db.Posts.Add(post);
+                    db.SaveChanges();
+
+                   
+                    return RedirectToAction("Index");
+                }
+
+            }
+
+            return View(post);
+        }
+
+        public ActionResult ViewAllComments()
+        {
+            using (var db = new ForumDbContext())
+            {
+                var comments = db.Posts
+                    .Include(c => c.Author)
+                    .ToList();
+                return View(comments);
+            }
+
+        }
+
+        [Authorize]
+        [HttpGet]
+        public ActionResult Comment()
+        {
+            return View();
+        }
+        [Authorize]
+        [HttpPost]
+        public ActionResult Comment(Comment comment)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db = new ForumDbContext())
+                {
+                    var authorId = User.Identity.GetUserId();
+                    comment.AuthorId = authorId;
+
+                    db.Comments.Add(comment);
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+            }
+
+            return View(comment);
+        }
+
+        public bool IsAuthorized(Post post)
+        {
+            var isAdmin = this.User.IsInRole("Admin");
+            var isAuthor = post.IsAuthor(this.User.Identity.GetUserId());
+
+            return isAdmin || isAuthor;
+        }
+    }
+}
