56590eab7ab974f7a064351709a03f5309553a70
Plotso <ilian_1999n@abv.bg>
2017-04-23 03:37:51 +0300

    Added 'Create' and 'View' post options and also the button for all posts to be displayed on a single page. Improved the post controller...

---
+++ Forum Project With Article Creation And View/ForumProject/Controllers/PostController.cs
@@ -0,0 +1,81 @@
+﻿using ForumProject.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ForumProject.Controllers
+{
+    public class PostController : Controller
+    {
+        // GET: Post
+        public ActionResult Index()
+        {
+            return RedirectToAction("AllPost");
+        }
+
+        public ActionResult AllPosts()
+        {
+            using (var db = new ForumDbContext())
+            {
+                var posts = db.Posts
+                    .Include(p => p.Author)
+                    .ToList();
+
+                return View(posts);
+            }
+        }
+
+        public ActionResult ViewPost (int? id)
+        {
+            using (var db = new ForumDbContext())
+            {
+                var post = db.Posts
+                    .Include(p => p.Author)
+                    .Where(p => p.Id == id)
+                    .First();
+
+                if (post == null)
+                {
+                    return HttpNotFound();
+                }
+
+                return View(post);
+            }
+
+        }
+
+        [Authorize]
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db = new ForumDbContext())
+                {
+                    var authorId = User.Identity.GetUserId();
+                    post.AuthorId = authorId;
+
+                    db.Posts.Add(post);
+                    db.SaveChanges();
+
+                   
+                    return RedirectToAction("Index");
+                }
+
+            }
+
+            return View(post);
+        }
+    }
+}
---
+++ Forum Project With Article Creation And View/ForumProject/Models/Post.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ForumProject.Models
+{
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(69)]
+        public string Topic { get; set; }
+
+        [Required]
+        [StringLength(2500)]
+        public string Content { get; set; }
+
+        [ForeignKey("Author")]
+        public string AuthorId { get; set; }
+
+        public virtual ApplicationUser Author { get; set; }
+    }
+}
