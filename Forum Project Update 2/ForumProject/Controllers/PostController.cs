835fb9365360189ecbcec8429df4d95b5a376b34
Plotso <ilian_1999n@abv.bg>
2017-04-25 01:42:03 +0300

    Updated comments option and improved its full usability (they can be seen below each post and furthermore each comment has post id so that it appears where it belongs)...Also made a few changes to the CSS...And fixed the problem where the date that was supposed to show when a post was written was actually showing the time at the moment.

---
+++ Forum Project Update 2/ForumProject/Controllers/PostController.cs
@@ -0,0 +1,136 @@
+﻿using ForumProject.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ForumProject.Controllers
+{
+    public class PostController : Controller
+    {
+        // GET: Post
+        public ActionResult Index()
+        {
+            return RedirectToAction("AllPosts");
+        }
+
+        public ActionResult AllPosts()
+        {
+            using (var db = new ForumDbContext())
+            {
+                var posts = db.Posts
+                    .Include(p => p.Author)
+                    .ToList();
+
+                return View(posts);
+            }
+        }
+
+        public ActionResult ViewPost (int? id)
+        {
+            var db = new ForumDbContext();
+
+
+                var post = db.Posts
+                    .Where(p => p.PostId == id)
+                    .FirstOrDefault();
+
+                if (post == null)
+                {
+                    return HttpNotFound();
+                }
+
+                return View(post);
+            
+
+        }
+
+        [Authorize]
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db = new ForumDbContext())
+                {
+                    var authorId = User.Identity.GetUserId();
+                    post.AuthorId = authorId;
+
+                    db.Posts.Add(post);
+                    db.SaveChanges();
+
+                   
+                    return RedirectToAction("Index");
+                }
+
+            }
+
+            return View(post);
+        }
+
+        public ActionResult ViewAllComments()
+        {
+            using (var db = new ForumDbContext())
+            {
+                var comments = db.Posts
+                    .Include(c => c.Author)
+                    .ToList();
+                return View(comments);
+            }
+
+        }
+
+        [Authorize]
+        [HttpGet]
+        public ActionResult Comment(int id)
+        {
+            Comment comment = new Comment
+            {
+                PostId = id 
+            };
+            return View(comment);
+        }
+        [Authorize]
+        [HttpPost]
+        public ActionResult Comment(Comment comment)
+        {
+            if (ModelState.IsValid)
+            {
+                var db = new ForumDbContext();
+
+                Comment dbComment = new Comment
+                {
+                    PostId = comment.PostId,
+                    Content = comment.Content,
+                    AuthorId = User.Identity.GetUserId()
+                };
+                
+                    db.Comments.Add(dbComment);
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                
+            }
+
+            return View(comment);
+        }
+
+        public bool IsAuthorized(Post post)
+        {
+            var isAdmin = this.User.IsInRole("Admin");
+            var isAuthor = post.IsAuthor(this.User.Identity.GetUserId());
+
+            return isAdmin || isAuthor;
+        }
+    }
+}
