014330686830195371cc9c57fb2446dfd1cad2cb
LiardaZZ <sashko_cv@abv.bg>
2017-04-28 10:11:39 +0300

    Fixed bugs, removed search bar and Categories. Added href to users but still no functionality

---
+++ Forum Project FInalizing Update/ForumProject/Models/UserInfoModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ForumProject.Models
+{
+    public class UserInfoModel
+    {
+        [Display(Name = "FullName")]
+        public string FullName { get; set; }
+
+        public string Email { get; set; }
+
+        public string UserName { get; set; }
+    }
+}
